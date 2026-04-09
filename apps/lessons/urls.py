1e8beef77871ebc734f08e3e8403de073f42ce8e
qwx <qwxiae@gmail.com>
2026-04-10 00:31:07 +0300

    move submit_view from app.lesson to app.submission

--- apps/lessons/urls.py
+++ apps/lessons/urls.py
@@ -5,5 +5,4 @@ app_name = "lessons"
 
 urlpatterns = [
     path("lessons/<str:lesson_id>/", views.lesson_view, name="lesson"),
-    path("lessons/<str:lesson_id>/submit/", views.submit_view, name="submit"),
 ]
