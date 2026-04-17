b4d72510eddc64850c05f35a9339afe802ea8d0c
qwx <qwxiae@gmail.com>
2026-04-17 19:23:47 +0300

    fix user seed; use set_password() method

--- apps/users/management/commands/seed_users.py
+++ apps/users/management/commands/seed_users.py
@@ -11,21 +11,23 @@ class Command(BaseCommand):
         instructor_role = Role.objects.get(name="instructor")
         user_data = [
             ("test_user", "test@test.com", "hr2htBeTryYQ", True),
-            ("jane_doe", "janedoe@test.com", "madotsuki", True),
-            ("john_d0e", "johndoe@test.com", "urotsuki", False),
-            ("annabe113", "annabelle@test.com", "PXQENDSEDTkr", False),
-            ("nadine_rami", "nadinerami@test.com", "NWyhpcVtF6rp", False),
+            ("jane_doe", "janedoe@test.com", "testpass123", True),
+            ("john_d0e", "johndoe@test.com", "testpass123", True),
         ]
 
         for username, email, password, is_instructor in user_data:
-            user, create = User.objects.get_or_create(
-                username=username, email=email, defaults={"password": password}
+            user, created = User.objects.get_or_create(
+                username=username, email=email
             )
 
-            if create and is_instructor:
-                user_role = UserRole.objects.create(user=user, role=instructor_role)
-                user_role.save()
-                self.stdout.write(f"Created user: {email}")
+            if created:
+                user.set_password(password)
+                user.save()
+                
+                if is_instructor:
+                    user_role = UserRole.objects.create(user=user, role=instructor_role)
+                    user_role.save()
+                    self.stdout.write(f"Created user: {email}")
             else:
                 self.stdout.write(f"User '{email}' already exists")
 
